31a3125aa1ba752360dd93273e214707b0783420
Marcelo Lopes Silva <marcelo.utfpr@me.com>
2012-06-30 19:52:15 -0300

    Adicionar Bibliotecas adicionais

---
+++ gestor de arquivos/src/com/m4rc310/estagio/gestor/arquivo/gui/MainFrame.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.m4rc310.estagio.gestor.arquivo.gui;
+
+/**
+ *
+ * @author tchulla
+ */
+public class MainFrame  {
+
+
+
+    /**
+     * TODO apagar    
+     * Os metodos abaixo foram gerados automáticamente com intuito de testar as funcionalidades 
+     * desta classe quando necessario.
+     */
+    
+    public static void main(String[] args) {
+        new MainFrame().teste();
+    }
+
+    private void teste(){
+        // Implementação de testes das funcionalidades.
+    }
+
+}
