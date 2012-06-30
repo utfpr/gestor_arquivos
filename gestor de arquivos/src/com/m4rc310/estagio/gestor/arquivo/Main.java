a1fa4ee34d80e43344a298a6e7c824d88821dc04
Marcelo Lopes Silva <marcelo.utfpr@me.com>
2012-06-30 19:37:59 -0300

    construção do projeto java

---
+++ gestor de arquivos/src/com/m4rc310/estagio/gestor/arquivo/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.m4rc310.estagio.gestor.arquivo;
+
+/**
+ *
+ * @author tchulla
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Main");
+    }
+}
